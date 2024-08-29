fe931e01a67ad4040d49c4dbf824954238f1c49a
errantProgrammer <errantprogrammer@gmail.com>
2024-08-29 18:14:28 -0500

    start python questions hacerrank

---
+++ Hackerrank/Python/arithmetic_operators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    suma=a+b
+    resta=a-b
+    multiplicacion=a*b
+    print(suma)
+    print(resta)
+    print(multiplicacion)
---
+++ Hackerrank/Python/print_function.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    str_number = ""
+    for i in range(n):
+        str_number+=str(i + 1)
+    print(str_number)
